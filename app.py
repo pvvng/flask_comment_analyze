bf6804cbccb6ea2adcc9e203e1b315010657ff46
김동우 <t6u80o@naver.com>
2024-10-01 23:47:42 +0900

    키워드 불용어 set 추가. 거를거 거르고 집어넣기

--- app.py
+++ app.py
@@ -3,13 +3,15 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import json
 # 띄어쓰기
-from pykospacing import Spacing
+# from pykospacing import Spacing
 # 형태소 분석
 from konlpy.tag import Okt
 # 감정 분석 함수 호출
 from utils.my_functions.get_sentiment import get_sentiment
 # 키워드 배열 반환함수 호출
 from utils.my_functions.get_keyword import get_keyword
+# 불용어 파일 호출
+from dataset.stopword import stopword
 # sentiment 모델 불러오기
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch.nn.functional as F
@@ -26,7 +28,7 @@ if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
 
 # Spacing 객체 생성
-spacing = Spacing()
+# spacing = Spacing()
 
 # okt 객체 생성
 okt = Okt()
@@ -44,6 +46,7 @@ def say_hello():
 # POST 요청을 처리할 API 엔드포인트
 @app.route('/receive_data', methods=['POST'])
 def receive_data():
+
     # 받은 파일 댓글 데이터 변환
     file = request.files['file']
     data = file.read().decode('utf-8')
@@ -51,6 +54,7 @@ def receive_data():
     # 딕셔너리로 변환
     parsed_data = json.loads(data)
 
+    # 반환할 js 객체 형태의 딕셔너리
     keyword_dict = {}
     sentiment_dict = {
         "positive" : 0,
@@ -71,29 +75,31 @@ def receive_data():
         if like_to_add <= 0: 
             like_to_add = 1
 
-        # 띄어쓰기 교정
-        # corrected_sentence = spacing(text)
-
         # 키워드 분석 결과 삽입
         keyword_result = []
         keyword_result.extend(get_keyword(okt, text))
         
-        for keyword in keyword_result :
-            if keyword in keyword_dict:
-                # 키워드가 이미 존재하면 기존 값에 추가
-                keyword_dict[keyword] += like_to_add
-            else:
-                # 키워드가 존재하지 않으면 새로 추가
-                keyword_dict[keyword] = like_to_add
+        for keyword in keyword_result:
+            if keyword not in stopword:
+                if keyword in keyword_dict:
+                    # 키워드가 이미 존재하면 기존 값에 추가
+                    keyword_dict[keyword] += like_to_add
+                else:
+                    # 키워드가 존재하지 않으면 새로 추가
+                    keyword_dict[keyword] = like_to_add
         
         # 감정 분석
         s_sentiment_result = get_sentiment(text, s_tokenizer, s_model, F, 'steam')
         ns_sentiment_result = get_sentiment(text, ns_tokenizer, ns_model, F, 'naver-shopping')
+
         # 가중합을 통한 최종 예측
         # 가중치 조정
-        final_positive = (0.9 * s_sentiment_result[0] + 0.1 * ns_sentiment_result[0])  
-        final_negative = (0.4 * s_sentiment_result[1] + 0.6 * ns_sentiment_result[1])
+        final_positive = (0.7 * s_sentiment_result[0] + 0.3 * ns_sentiment_result[0])  
+        final_negative = (0.6 * s_sentiment_result[1] + 0.4 * ns_sentiment_result[1])
+
+        # 기본값 설정
         sentiment_result = "neutral"
+
         # 합산 검사
         diff = abs(final_positive - final_negative)
 
